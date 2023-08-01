f20065c7eaa89874da6f919ab7c9ba88dbe33c74
Jaskarandeep Singh <109353553+JKD03@users.noreply.github.com>
2023-08-01 15:48:42 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,7 +14,6 @@ const navLink = document.querySelectorAll('.nav__link')
 
 function linkAction(){
     const navMenu = document.getElementById('nav-menu')
-    // When we click on each nav__link, we remove the show-menu class
     navMenu.classList.remove('show')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
@@ -40,7 +39,6 @@ window.addEventListener('scroll', scrollActive)
 
 function scrollUp(){
     const scrollUp = document.getElementById('scroll-up');
-    // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
     if(this.scrollY >= 560) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
 }
 window.addEventListener('scroll', scrollUp)
